ace573a2d8e7a6d42fc5955c42ca3df871a6a794
john-galvao <129496776+john-galvao@users.noreply.github.com>
2023-04-12 16:10:36 -0400

    application and main

---
+++ application.rb
@@ -0,0 +1,33 @@
+module Application
+    def self.run
+        menu = Menu.new
+        begin
+puts " Simple Note Application ".center(50, "#")
+puts "a) Add"
+puts "b) Edit"
+puts "c) Delete"
+puts "d) Show All"
+puts "q) Quit"
+print "Select: "
+choice = gets.chomp
+
+case choice
+when 'a' then menu.add
+end
+
+        end while choice != 'q'
+    end
+
+
+    class Menu
+        def initialize
+
+        end
+
+        def add
+
+        end
+
+    end
+
+end
