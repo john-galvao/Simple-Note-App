b017c307c375b894c588ca41fcf413a977705c39
john-galvao <129496776+john-galvao@users.noreply.github.com>
2023-04-12 16:19:05 -0400

    manager object

---
+++ manager.rb
@@ -0,0 +1,9 @@
+# Manager class will operate all operations over the database class
+# We need a manager object
+# The manager object will need a database object
+
+class Manager
+    def initialize
+
+    end
+end
