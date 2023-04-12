d8671dfb1ecffa85db887c591dfa9f332026d9e1
john-galvao <129496776+john-galvao@users.noreply.github.com>
2023-04-12 16:24:21 -0400

    database instance

--- manager.rb
+++ manager.rb
@@ -1,9 +1,10 @@
 # Manager class will operate all operations over the database class
 # We need a manager object
 # The manager object will need a database object
+require './database'
 
 class Manager
     def initialize
-
+        @database = Database.new
     end
 end
