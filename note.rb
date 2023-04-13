1d72cc5a723a376b757618fe96fc7be08a77b7a2
john-galvao <129496776+john-galvao@users.noreply.github.com>
2023-04-12 23:17:10 -0400

    override str method

--- note.rb
+++ note.rb
@@ -2,10 +2,14 @@ require 'securerandom'
 
 class Note
     attr_reader :id
-    
+
     def initialize(text)
         @id = SecureRandom.uuid
         @text = text
         @date = Time.now
+    end
+
+    def to_s
+        "Date: #{@date.strftime("%Y-%m-%d %H:%M:%S")} \nNote: #@text"
     end
 end
